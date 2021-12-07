02d0d82a255ac25f7a601ea655f1d6ebea622fb3
Yurii <yuriikuzhil@gmail.com>
2021-12-07 19:36:12 +0200

    copied team project

---
+++ src/js/customer-slider.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
